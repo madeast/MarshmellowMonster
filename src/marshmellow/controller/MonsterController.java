9695c9a9147a5c17874208d71433208ba317c555
madeast <emad6932@740s-pl-113.local>
2015-10-05 09:50:46 -0600

    Added more popups.

--- src/marshmellow/controller/MonsterController.java
+++ src/marshmellow/controller/MonsterController.java
@@ -11,21 +11,20 @@ public class MonsterController
 	private MarshmallowMonster userMonster;
 	private MarshmallowOutput myOutput;
 	private Scanner monsterScanner;
-	
-	
+
 	public MonsterController()
 	{
-		int eyes = 4;
+		int Eyes = 4;
 		double legs = 8;
 		int nose = 1;
 		double hair = 2;
 		boolean hasBellyButton = false;
 		String name = "Spindle the Spider";
-		
+
 		monsterScanner = new Scanner(System.in);
 		myOutput = new MarshmallowOutput();
-		ashMonster = new MarshmallowMonster(name, eyes, nose, hair, legs, hasBellyButton);	
-		
+		ashMonster = new MarshmallowMonster(name, eyes, nose, hair, legs, hasBellyButton);
+
 	}
 
 	public void start()
@@ -34,35 +33,48 @@ public class MonsterController
 		myOutput.displayMonsterInfo(ashMonster.toString());
 		myOutput.displayMonsterGUI(ashMonster.toString());
 		myOutput.displayMonsterName(ashMonster.toString());
-		//this.makeUserMonster();
-		//myOutput.displayMonsterInfo("New Monster Info" + userMonster.toString());
+		// this.makeUserMonster();
+		// myOutput.displayMonsterInfo("New Monster Info" +
+		// userMonster.toString());
 	}
-	
+
 	private void askQuestions()
 	{
 		String betterMonsterName = myOutput.grabAnswer("Type in a better name.");
 		myOutput.showResponse("You typed in: " + betterMonsterName);
+
 		String betterMonsterEyes = myOutput.grabAnswer("Type in how many eyes the monster have.");
-		while(!isInteger(betterMonsterEyes))
+		int inputEyes;
+		
+		while (!isInteger(betterMonsterEyes))
 		{
-			betterMonsterEyes = myOuput.grabAnswer("Type in a positive integer for the monster eyes.")
+			betterMonsterEyes = myOutput.grabAnswer("Type in a positive integer for the monster eyes.");
 		}
 		
+		if (!isInteger(betterMonsterEyes))
+		{
+			inputEyes = Integer.parseInt(betterMonsterEyes);
+		}
+		else
+		{
+			inputEyes = -999;
+		}
+
 		System.out.println("Type in a new number of eyes");
 		int betterMonsterEyes = monsterScanner.nextInt();
-		
+
 		System.out.println("Type in a new number of legs");
 		double betterMonsterLegs = monsterScanner.nextDouble();
-		
+
 		System.out.println("Type in a new number of noses");
 		int betterMonsterNose = monsterScanner.nextInt();
-		
+
 		System.out.println("Type in a new number of hair");
 		double betterMonsterHair = monsterScanner.nextDouble();
-		
+
 		System.out.println("Will it have a bellybutton?(True/False)");
 		boolean betterMonsterBellyButton = monsterScanner.nextBoolean();
-		
+
 		ashMonster.setMonsterName(betterMonsterName);
 		ashMonster.setMonsterEyes(betterMonsterEyes);
 		ashMonster.setMonsterLegs(betterMonsterLegs);
@@ -72,19 +84,20 @@ public class MonsterController
 	}
 
 	/**
-	 * This method will get the information to create an instance of a MarshmallowMonster.
+	 * This method will get the information to create an instance of a
+	 * MarshmallowMonster.
 	 */
-	private void makeUserMonster()
+	/*private void makeUserMonster()
 	{
-		//Step one: Get variables
+		// Step one: Get variables
 		String userName;
 		int userEyes;
 		int userNoseCount;
 		double userHair;
 		double userLegs;
 		boolean userBellyButton;
-		
-		//Step two:Define variables by using Scanner to get inputs
+
+		// Step two:Define variables by using Scanner to get inputs
 		System.out.println("Type in your name for your monster.");
 		userName = monsterScanner.nextLine();
 		System.out.println("Type in the number of eyes for your monster.");
@@ -97,11 +110,26 @@ public class MonsterController
 		userLegs = monsterScanner.nextDouble();
 		System.out.println("Does it have a bellybutton?(True/False)");
 		userBellyButton = monsterScanner.nextBoolean();
-		
-		//Step three: Make a monster - use the Constructor!
+
+		// Step three: Make a monster - use the Constructor!
 		userMonster = new MarshmallowMonster(userName, userEyes, userNoseCount, userHair, userLegs, userBellyButton);
+
+	}*/
+	
+	private boolean isInteger(String input)
+	{
+		boolean isInt = false;
 		
+		try
+		{
+			int inputEyes = Integer.parseInt(input);
+			isInt = true;
+		}
+		catch (NumberFormatException error)
+		{
+			myOutput.showResponse("Not a valid Integer - bad value will be used.");
+		}
 		
-		
+		return isInt;
 	}
 }
